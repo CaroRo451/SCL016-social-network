73c6b0fe71aa7b8f6fa72ce42d8864b17cac9384
lia-serrano <liaserranolopez@gmail.com>
2021-05-26 12:43:33 -0400

    mainview

--- src/views/mainView.js
+++ src/views/mainView.js
@@ -1,16 +1,21 @@
 export const mainView = () => {
+
   const navProfile = document.createElement('nav');
+
   const viewProfile = ` 
         <ul>Logo Pagina</ul>
         <ul>Menu Hamb</ul>
    `
   navProfile.innerHTML = viewProfile;
 
+
   return navProfile;
 };
 
 export const movePost = () => {
+
   const navPost = document.createElement('nav');
+
   const viewPost = ` 
         <ul>Like</ul>
         <ul>Editar Post</ul>
